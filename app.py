152c1a81d0f1001e881759de420b133a3d47b190
willkretschmer <willkretschmer@yahoo.com>
2025-07-08 11:47:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,7 @@ def nlp():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
